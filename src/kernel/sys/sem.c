002ea8125275d527080c86e5b02dfa5b4239cca7
Loris Mottino <loris.mottino@etu.univ-grenoble-alpes.fr>
2022-02-16 11:33:07 +0100

    Ca marche pas encore mais on avance

--- src/kernel/sys/sem.c
+++ src/kernel/sys/sem.c
@@ -2,24 +2,111 @@
 #include <nanvix/const.h>
 
 
+// Structure de sémaphore
 struct sem {
-	unsigned state;
-	unsigned count;
+	unsigned state; // Etat
+	unsigned count; // Nombre de places disponibles
+	unsigned key;   // Clé associée à la sémaphore
 
-	struct process **queue;
+	struct process **queue; // Processus en file d'attente
 };
 
 
+// Nombre maximal de sémaphores
 #define SEM_MAX 64
 
+// Définition de la première et de la dernière sémaphore (pointeurs de structure)
+#define FIRST_SEM ((&semtab[0]))
+#define LAST_SEM ((&semtab[SEM_MAX - 1]))
 
+// Etats possibles des sémaphores
+#define SEM_IDLE 0
+#define SEM_ACTIVE 1
+
+
+// Tableau des structures sémaphores (l'ID d'une sémaphore est sa place dans le tableau)
 PUBLIC struct sem semtab[SEM_MAX];
-// Table des semaphores est statique
 
+// Nombre de sémaphores actives
 PUBLIC unsigned nsems = 0;
 
 
 
+// Initialisation des sémaphores du tableau à leur état initial
 PUBLIC void sem_init(void) {
+	struct sem *s
 
+	for (s = FIRST_SEM; s <= LAST_SEM; s++) {
+		s->state = SEM_IDLE;
+		s->count = 0;
+		s->key = 0;
+		s->queue = NULL;
+	}
+}
+
+
+int sem_create(int semid, unsigned key, int n) {
+	if (semid < 0 || semid >= SEM_MAX)
+		return -1;
+
+	struct sem *s = semtab[semid];
+
+	if (s->state == SEM_ACTIVE)
+		return -1;
+
+	s->state = SEM_ACTIVE
+	s->count = n;
+	s->key = key;
+	s->queue = NULL;
+
+	return semid;
+}
+
+int sem_down(int semid) {
+	if (semid < 0 || semid >= SEM_MAX)
+		return -1;
+
+	struct sem *s = semtab[i];
+
+	if (s->state == SEM_IDLE)
+		return -1;
+
+	if (s->count == 0)
+		sleep(s->queue, curr_proc->priority);
+	else
+		s->count--;
+
+	return semid;
+}
+
+int sem_up(int semid) {
+	if (semid < 0 || semid >= SEM_MAX)
+		return -1;
+
+	struct sem *s = semtab[i];
+
+	if (s->state == SEM_IDLE)
+		return -1;
+
+	wakeup(s->queue);
+	s->count++;
+
+	return semid;
+}
+
+int destroy(int semid) {
+	if (semid < 0 || semid >= SEM_MAX)
+		return -1;
+
+	struct sem *s = semtab[i];
+
+	if (s->state == SEM_IDLE)
+		return -1;
+
+	s->state = SEM_IDLE;
+	s->count = 0;
+	s->key = 0;
+	s->queue = NULL;
+
+	return semid;
 }
--- src/kernel/sys/sem/semget.c
+++ src/kernel/sys/sem/semget.c
@@ -4,7 +4,28 @@
 
 
 PUBLIC int sys_semget(unsigned key) {
-	// Not yet implemented
+	struct sem *s;
+	int i, first_idle = -1;
 
-	return -1;
+	for (s = FIRST_SEM, i = 0; s <= LAST_SEM; s++, i++) {
+		if (s->state == SEM_IDLE && first_idle == -1)
+			first_idle = i;
+		if (s->key == key)
+			break;
+	}
+
+	// Si aucune sémaphore ne correspond à la clé, on active une sémaphore
+	if (i == SEM_MAX) {
+		// S'il n'y a plus de sémaphores disponibles, erreur (-1)
+		if (first_idle == -1)
+			return -1;
+		
+		// S'il reste une sémaphore disponible, on l'active et on retourne son identifiant
+		else
+			return sem_create(first_idle, key, 1);
+	}
+	
+	// Si une sémaphore contenant la clé demandée a été trouvée, on retourne son identifiant
+	else
+		return i;
 }
